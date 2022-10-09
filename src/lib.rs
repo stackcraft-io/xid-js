f230b50220f9008064bd497d1932bfaa51d1b83d
dong.huo <huodon@gmail.com>
2022-10-09 13:00:21 +0800

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![deny(clippy::all)]
 
-use napi::{bindgen_prelude::Array, Env};
-
 #[macro_use]
 extern crate napi_derive;
 
@@ -11,10 +9,11 @@ pub fn one() -> String {
 }
 
 #[napi]
-pub fn many(env: Env, size: u32) -> Array {
-  let mut result = env.create_array(size).unwrap();
-  for i in 0..size {
-    result.set::<String>(i, xid::new().to_string()).unwrap();
+pub fn many(size: u32) -> Vec<String> {
+  let mut result = Vec::with_capacity(size as usize);
+  for _ in 0..size {
+    result.push(xid::new().to_string());
   }
+
   result
 }
